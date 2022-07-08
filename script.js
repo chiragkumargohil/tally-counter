5929b992479d0dab7798c0b6d3db174029d0e0f1
Chiragkumar Gohil <108747654+chiragkumargohil@users.noreply.github.com>
2022-07-08 18:19:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 document.getElementById("display").innerHTML = 0;
+
+var count = 0;
+
+function decreament() {
+    count = count - 1;
+    document.getElementById("display").innerHTML = count;
+}
+
+function increament() {
+    count = count + 1;
+    document.getElementById("display").innerHTML = count;
+}
+
+function reset() {
+    count = 0;
+    document.getElementById("display").innerHTML = count;
+}
